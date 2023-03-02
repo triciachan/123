06d08116a45a03d46388cdeeb6d086f6d97b63a4
Chu-Ching Huang <cchuang2009@gmail.com>
2023-03-02 22:10:06 +0800

    replace talib by pandas_ta

--- streamlit_bakeh_1.py
+++ streamlit_bakeh_1.py
@@ -9,14 +9,11 @@ import plotly.express as ex
 # data utitilies
 import yfinance as yf
 import pandas as pd
-import pandas_ta
+import pandas_ta as ta
 import datetime as dt
 
 # streamlit artiface 
 import streamlit as st
-
-# utility for technical indexes
-import talib
 
 # Backtest
 from backtesting import Backtest, Strategy
@@ -35,24 +32,17 @@ df = yf.download(ticker, start = start, end = end)
 
 # More technic indexes
 
-df['stoch_k'] = pandas_ta.stochrsi(close=df['Adj Close'],length=20).iloc[:,0]
-df['stoch_d'] = pandas_ta.stochrsi(close=df['Adj Close'],length=20).iloc[:,1]
-df['bb_lower'] = pandas_ta.bbands(close=df['Adj Close'],length=20).iloc[:,0]
-df['bb_upper'] = pandas_ta.bbands(close=df['Adj Close'],length=20).iloc[:,2]
+df['stoch_k'] = ta.stochrsi(close=df['Adj Close'],length=20).iloc[:,0]
+df['stoch_d'] = ta.stochrsi(close=df['Adj Close'],length=20).iloc[:,1]
+df['bb_lower'] = ta.bbands(close=df['Adj Close'],length=20).iloc[:,0]
+df['bb_upper'] = ta.bbands(close=df['Adj Close'],length=20).iloc[:,2]
 df['forward_1d'] = df['Adj Close'].pct_change(1).shift(-1)
 
-#fig=ex.line(df, x=df.index, y=['Adj Close','bb_lower','bb_upper'], title='台積電 (2330.TW) Adj Close with Bollinger Bands')
-#fig.update_layout(title_text=f'台積電 (2330.TW) Adj Close with Bollinger Bands', title_x=0.5);
+fig=ex.line(df, x=df.index, y=['Adj Close','bb_lower','bb_upper'], title='台積電 (2330.TW) Adj Close with Bollinger Bands')
+fig.update_layout(title_text=f'台積電 (2330.TW) Adj Close with Bollinger Bands', title_x=0.5);
 
 # Plot!
-#st.plotly_chart(fig, use_container_width=True)
-
-
-
-
-# compute simple moving average using talib
-sma1 = talib.SMA(df['Adj Close'], timeperiod=20)
-sma2 = talib.SMA(df['Adj Close'], timeperiod=60)
+st.plotly_chart(fig, use_container_width=True)
 
 
 class Strategy(Strategy):
@@ -65,8 +55,8 @@ class Strategy(Strategy):
         
         # Precompute the two moving averages
         close = pd.Series(self.data.Close)
-        sma1 = talib.SMA(close, timeperiod=self.n1)
-        sma2 = talib.SMA(close, timeperiod=self.n2)
+        sma1 = ta.sma(close,self.n1)
+        sma2 = ta.sma(close,self.n2)
 
         # Precompute signal
         signal_long = (sma1 > sma2) & (sma1.shift() < sma2.shift())
